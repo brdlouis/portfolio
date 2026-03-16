28f0b8015204f1f4834c2eed2552bb20fe2ac3fa
Louis Bordes <louis.bordes@etu.uca.fr>
2026-03-16 22:00:49 +0100

    remise a niveau

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  // 1. Ajoutez votre URL GitHub Pages (indispensable pour le sitemap et les métadonnées)
   site: 'https://brdlouis.github.io',
 
-  // 2. Ajoutez le nom de votre dépôt GitHub (ex: '/mon-portfolio')
-  // SI votre site est sur 'votre-pseudo.github.io' (dépôt principal), mettez '/'
   base: '/portfolio',
 
   vite: {
